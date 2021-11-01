b7c6f97ea708b68b025de87f8d088a8474e1d6d9
lucas <lucas.apaulus012@gmail.com>
2021-11-01 15:59:49 -0300

    Alterei os if e coloque if else no lugar

--- snapCrackle.js
+++ snapCrackle.js
@@ -1,23 +1,32 @@
-function snapCrackle(maxValue){
-    let word = []
-    for (let value = 1; value <= maxValue; value++){
-        if (value % 2 !== 0){
-            
-            word.push('Snap ')
-        }
-        if (value % 5 == 0){
-            
-            word.push('Crackle ')
-        }
-        if (value % 5 == 0 && value % 2 !== 0){
 
-            word.push('SnapCrackle ')
-        }
-        if (value % 5 !== 0 && value % 2 == 0){
-
-            word.push(value)
-        }
+// Fiz uma mudança, coloquei else if no lugar dos if, agora deu certo.
+function snapCrackle(maxValue) {
+    let word = [];
+    for (let value = 1; value <= maxValue; value++) {
         
-    }
-    return word
+        if (value % 2 !== 0) {
+            word.push(" Snap ");
+
+        } 
+
+        else if (value % 5 === 0) {
+            word.push(" Crackle ");
+        }   
+
+        else if (value % 5 === 0 && value % 2 !== 0) {
+            word.push(" SnapCrackle ");
+        }
+            
+        
+        else if (value % 5 !== 0 && value % 2 == 0) {
+            word.push(" " + value + " ");
+        } 
+        
+        
+        
+      }
+
+      return word.join()
 }
+
+console.log(snapCrackle(12))
