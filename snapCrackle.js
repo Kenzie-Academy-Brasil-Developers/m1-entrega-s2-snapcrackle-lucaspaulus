faa6ba96a964b8ba71530e9b1d504d3e93ce8b3f
lucas <lucas.apaulus012@gmail.com>
2021-11-03 16:50:56 -0300

    Alterei a ordem dos if e else if

--- snapCrackle.js
+++ snapCrackle.js
@@ -3,8 +3,11 @@
 function snapCrackle(maxValue) {
     let word = [];
     for (let value = 1; value <= maxValue; value++) {
-        
-        if (value % 2 !== 0) {
+        if (value % 5 === 0 && value % 2 !== 0) {
+            word.push(" SnapCrackle ");
+        }
+
+        else if (value % 2 !== 0) {
             word.push(" Snap ");
 
         } 
@@ -12,17 +15,10 @@ function snapCrackle(maxValue) {
         else if (value % 5 === 0) {
             word.push(" Crackle ");
         }   
-
-        else if (value % 5 === 0 && value % 2 !== 0) {
-            word.push(" SnapCrackle ");
-        }
-            
         
         else if (value % 5 !== 0 && value % 2 == 0) {
             word.push(" " + value + " ");
         } 
-        
-        
         
       }
 
